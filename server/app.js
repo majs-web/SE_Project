413ac95994c940d66f046301ceefb842660502f4
Maja Lie <maja.lie@code.berlin>
2026-03-06 17:48:38 +0100

    Import middleware to app.js. Try example test routes

--- server/app.js
+++ server/app.js
@@ -1,12 +1,29 @@
 import express from 'express'
+import { logger } from '../middlewares/logger.js'
 
 const app = express()
 const PORT = 3000
 
-app.get('/', (request, response) => {
-    response.send('Welcome to Bondeappen!')
+app.use(logger)
+
+const name = ''
+
+app.get('/maths', (request, response) => {
+    const num1 = 10
+    const num2 = 20
+    const num3 = 15
+    const average = (num1 + num2 + num3) / 3
+    response.send(`The average of ${num1}, ${num2}, and ${num3} is ${average}!`)
+})
+
+app.post('/about', (request, response) => {
+    response.send('Reach out if you have a question.')
+})
+
+app.get('/legal', (request, response) => {
+    response.send(`Hi, ${name}! Welcome to this page. Today's dinner: ${favFood}`)
 })
 
 app.listen(PORT, () => {
-    console.log('Started server on port ${PORT}')
+    console.log(`Started server on port ${PORT}`)
 })
