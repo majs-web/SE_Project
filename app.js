d2a8c2cc023738e1bd53fd3eb95013808fbfe1c8
Maja Lie <maja.lie@code.berlin>
2026-03-08 21:04:20 +0100

    Import articles from mock database. Set routes to ensure navigation between static and dynamic pages. Add slug with conditional

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-import express from 'express'
-import { logger } from './middlewares/logger.js'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { logger } from './middlewares/logger.js';
+import { newsArticles } from './data/news.js';
 
-const app = express()
-const PORT = 3000
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }))
-app.use(logger)
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = 3000;
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+
 
 app.get('/', (request, response) => {
-    response.send('Welcome to Bondeappen!')
+    response.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-app.get('/about', (request, response) => {
-    console.log(request.query)
 
-    response.send('Here you soon find information about us!')
+app.get('/news', (request, response) => {
+    response.render('news', { articles: newsArticles })
+})
+
+app.get('/news/:slug', (request, response) => {
+    const article = newsArticles.find(a => a.slug === request.params.slug);
+    if (article) {
+        response.render('article', { article });
+    } else {
+        res.status(404).send('Article not found');
+    }
+});
+
+
+app.get('/about', (request, response) => {
+    response.sendFile(path.join(__dirname, 'public', 'about.html'));
 })
 
 app.post('/about', (request, response) => {
     console.log('Contact form submission: ', request.body)
-    response.send("Thank you for reaching out - we'll get back to you soon.")
+    response.redirect("/about.html?success=1")
 })
 
+
 app.get('/legal', (request, response) => {
-    response.send('Here you find the legal stuff.')
+    response.sendFile(path.join(__dirname, 'public', 'legal'));
 })
+
 
 app.listen(PORT, () => {
     console.log(`Started server on port ${PORT}`)
