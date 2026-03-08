50985b11f173635762cbb4a5182bae45af34b2cf
Maja Lie <maja.lie@code.berlin>
2026-03-08 14:12:07 +0100

    Edit ap.js to fit the new .js files paths

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 import express from 'express'
-import { logger } from '../middlewares/logger.js'
-import path from 'path';
-import { fileURLToPath} from 'url'
+import { logger } from './middlewares/logger.js'
 
 const app = express()
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(logger)
 
@@ -25,6 +21,10 @@ app.get('/about', (request, response) => {
 app.post('/about', (request, response) => {
     console.log('Contact form submission: ', request.body)
     response.send("Thank you for reaching out - we'll get back to you soon.")
+})
+
+app.get('/legal', (request, response) => {
+    response.send('Here you find the legal stuff.')
 })
 
 app.listen(PORT, () => {
