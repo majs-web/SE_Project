fdf5b24b6c64e2616853d3ecade48449dee00e91
Maja Lie <maja.lie@code.berlin>
2026-03-08 21:01:12 +0100

    Add mock database with test articles for news page

---
+++ data/news.js
@@ -0,0 +1,37 @@
+/* export const news = [
+    { name: 'Article 1', slug: 'article-1' },
+    { name: 'Article 2', slug: 'article-2'}
+] */
+
+export const newsArticles = [
+{
+    slug: 'article-1',
+    title: 'Article 1',
+    date: '01.01.2026',
+    summary: 'Example article 1',
+    body: `
+    This is a test to see if it works to export these
+    articles and use this document.
+    `,
+},
+{
+    slug: 'article-2',
+    title: 'Article 2',
+    date: '01.02.2026',
+    summary: 'Example article 2',
+    body: `
+    This is a test to see if it works to export these
+    articles and use this document.
+    `,
+},
+{
+    slug: 'article-3',
+    title: 'Article 3',
+    date: '01.03.2026',
+    summary: 'Example article 3',
+    body: `
+    This is a test to see if it works to export these
+    articles and use this document.
+    `,
+}
+]
