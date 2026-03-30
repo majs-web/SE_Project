cb85cca4b78ed47c23e7777a81025c84c9d98fe0
Maja Lie <maja.lie@code.berlin>
2026-03-31 00:52:34 +0200

    Edit schema for news article data

---
+++ models/news.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const articleSchema = new mongoose.Schema({
+    slug: { type: String, unique: true, required: true},
+    title: { type: String, required: true},
+    date: { type: Date, required: true},
+    summary: { type: String, required: true},
+    body: { type: String, required: true}
+});
+
+export const newsArticles = mongoose.model('Article', articleSchema);
