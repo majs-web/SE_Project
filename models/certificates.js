56242e5babe3a676f2f9f885a410b1b9d35205bf
Maja Lie <maja.lie@code.berlin>
2026-03-31 00:51:47 +0200

    Fix typos

--- models/certificates.js
+++ models/certificates.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const certificateSchema = new mongoose.Schema({
     slug: { type: String, unique: true, required: true},
     name: { type: String, required: true},
-    description: { type: String, requried: true},
-    isInStock: { type: Boolean, default: true, required: true}
+    description: { type: String, required: true},
+    isInStock: { type: Boolean, default: true}
 });
 
 export const Certificate = mongoose.model('Certificate', certificateSchema);
