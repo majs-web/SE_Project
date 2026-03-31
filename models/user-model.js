b57253311a7387982f1d38605cbbca328559e4b8
Maja Lie <maja.lie@code.berlin>
2026-03-31 16:50:18 +0200

    Delete config folder with app.js and database.js, as well as src/services: unused files

--- models/user-model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-    useName: { type: String, unique: true, required: true},
-    password: { type: String, required: true},
-});
-
-export const User = mongoose.model('User', userSchema);
