6b8b50e82cd9e98fbec56faa9d6a81bbb61a064c
Maja Lie <maja.lie@code.berlin>
2026-03-31 16:06:06 +0200

    Fix typo

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = 3000;
 
 mongoose.connect('mongodb://127.0.0.1:27017/bondeappen')
-    .then(() => console.log('Database connectetd'))
+    .then(() => console.log('Database connected'))
     .catch(error => console.error(error))
 
 // Config 
