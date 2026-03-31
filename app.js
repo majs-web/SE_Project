e2a8d255c7340fc0ecdf3b94666c3120b5a78550
Maja Lie <maja.lie@code.berlin>
2026-03-31 15:25:59 +0200

    Edit app.use(session) to function with current login routing

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
-import express, { response } from 'express';
+import express from 'express';
 import { logger } from './middlewares/logger.js';
 
 import mongoose from 'mongoose';
 
 import session from 'express-session';
-import authRoutes from './controllers/auth.js'
+import authRoutes from './controllers/auth.js';
 
 // Import controllers
 import simpleRoutes from './controllers/simple-pages.js';
@@ -31,13 +31,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
 app.use(session({
-    name: 'MySessionID',
-    secret: process.env.SECRET || 'dev_secret',
+    secret: 'dev_secret',
     resave: false,
     saveUninitialized: false,
-    cookie: {
-        httpOnly: false //only works false when localhost
-    }
 }));
 
 // Controllers
@@ -49,31 +45,4 @@ app.use(certificateRoutes);
 
 app.listen(PORT, () => {
     console.log(`Started server on port ${PORT}`);
-})
-
-// db connection
-/* let db
-connectToDb((err) => {
-    if (!err) {
-        app.listen(3000, () => {
-            console.log(`Started server on port 3000`);
-        })
-        db = getDb()
-    }
-}) */
-
-// Test with book example
-/* app.get('/books', (request, response) => {
-    let books = [] // Where the books get stored after retreiving from db
-
-    db.collection('books')
-        .find() // .find() returns cursor, can use two methods, toArray and forEach -> brings back different 
-        .sort({ author: 1 })
-        .forEach(book => books.push(book)) // Iterate each book individually
-        .then(() => {
-            response.status(200).json(books)
-        })
-        .catch(() => {
-            response.status(500).json({error: 'Could not fetch the documents'})
-        })
-}) */
+});
