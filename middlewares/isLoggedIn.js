dcb922f136d995793561001ab838188d39e52353
Maja Lie <maja.lie@code.berlin>
2026-03-31 17:10:25 +0200

    Add comment

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,5 +1,6 @@
 
 // Authentication middleware
+// NB: Used ChatGPT to figure out how to create this middleware
 export function isLoggedIn(request, response, next) {
     if (!request.session.username) {
         return response.redirect('/login');
